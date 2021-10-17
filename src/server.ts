3444c5430fc3ffd830a30d8e49c0d1270cc4183e
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-10-17 11:16:25 +1100

    Create a config file - cleanup

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ export const start = async () => {
   try {
     app.listen(app.get("port"), () => {
       console.log("REST API is on http://localhost:%d/api", app.get("port"));
-      console.log("port", app.get("port"), process.env.PORT);
     });
   } catch (e) {
     console.error(e);
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -5,7 +5,6 @@ export const hydrateEnv = () => {
   if (fs.existsSync(".env")) {
     // TODO: Enter Logger here
     dotenv.config({ path: ".env" });
-    console.log("hydrate env", process.env.PORT);
   } else {
     // TODO: Enter Logger here
     throw new Error("Please provide an .env file");
