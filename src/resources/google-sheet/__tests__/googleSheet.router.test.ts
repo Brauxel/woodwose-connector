51ff3e71df319488309a621c6319352ef1582483
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-11-07 22:51:27 +1100

    Sheet ID updatd in tests

--- src/resources/google-sheet/__tests__/googleSheet.router.test.ts
+++ src/resources/google-sheet/__tests__/googleSheet.router.test.ts
@@ -4,7 +4,7 @@ import app from "../../../app";
 describe("GET /api/google-sheet/:id", () => {
   it("should return a 200 response", async () => {
     const response = await request(app).get(
-      "/api/google-sheet/1LsZQD67Lj166_5Hh7-BY6sKS8vKEwDCylxmmtUOIilY"
+      "/api/google-sheet/1Bgyxk10XF4JXdB-4XpEDozkEtdfw-ET_JAzSBFqdMmo"
     );
     expect(response.statusCode).toBe(200);
     expect(response.body.data.length).toBeGreaterThanOrEqual(1);
@@ -14,7 +14,7 @@ describe("GET /api/google-sheet/:id", () => {
 describe("PUT /api/google-sheet/:id", () => {
   it("should return a 200 response", async () => {
     const response = await request(app).put(
-      "/api/google-sheet/1LsZQD67Lj166_5Hh7-BY6sKS8vKEwDCylxmmtUOIilY"
+      "/api/google-sheet/1Bgyxk10XF4JXdB-4XpEDozkEtdfw-ET_JAzSBFqdMmo"
     );
     expect(response.statusCode).toBe(200);
   });
