0f169fee2cdc0ff80a2ed2fb681cc1f87201b0b1
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-04-23 21:08:25 +1000

    [Aakash Bhatia]-[A1VO0CXV]-[test added for create woo commerce api]

--- src/utils/wordpress/wooCommerce/__tests__/wooCommerce.test.ts
+++ src/utils/wordpress/wooCommerce/__tests__/wooCommerce.test.ts
@@ -3,42 +3,67 @@ import { createWooCommerceApi } from "../wooCommerce";
 
 describe("WordPress Utils - WooCommerce", () => {
   describe("createWooCommerceApi", () => {
-    beforeAll(() => {
-      hydrateEnv();
+    describe("Error", () => {
+      it("should return error if ENV values are not provided", () => {
+        expect(() => {
+          createWooCommerceApi();
+        }).toThrowError("Could not create Woo Commerce Api");
+      });
     });
 
-    it("should create a WooCommerce API with default values in the ENV file when no custom arguments are provided", () => {
-      const WooCommerce = createWooCommerceApi();
-
-      expect(WooCommerce).toMatchObject({
-        classVersion: "1.0.1",
-        url: "https://woodwose.in",
-        wpAPIPrefix: "wp-json",
-        version: "wc/v3",
-        isHttps: true,
-        encoding: "utf8",
-        queryStringAuth: false,
-        port: "",
-        timeout: undefined,
-        axiosConfig: {},
+    describe("Success", () => {
+      beforeAll(() => {
+        hydrateEnv();
       });
 
-      expect(WooCommerce.consumerKey).toBeDefined();
-      expect(WooCommerce.consumerSecret).toBeDefined();
-    });
+      it("should create a WooCommerce API with default values in the ENV file when no custom arguments are provided", () => {
+        const WooCommerce = createWooCommerceApi();
 
-    it("should create a WooCommerce API Object with custom values", () => {
-      const WooCommerce = createWooCommerceApi(
-        "https://test-url.com",
-        "test consumer key",
-        "test consumer secret"
-      );
+        expect(WooCommerce).toMatchObject({
+          classVersion: "1.0.1",
+          url: "https://woodwose.in",
+          wpAPIPrefix: "wp-json",
+          version: "wc/v3",
+          isHttps: true,
+          encoding: "utf8",
+          queryStringAuth: false,
+          port: "",
+          timeout: undefined,
+          axiosConfig: {},
+        });
 
-      expect(WooCommerce).toMatchObject({
-        url: "https://test-url.com",
-        consumerKey: "test consumer key",
-        consumerSecret: "test consumer secret",
+        expect(WooCommerce.consumerKey).toBeDefined();
+        expect(WooCommerce.consumerSecret).toBeDefined();
+      });
+
+      it("should create a WooCommerce API Object with custom values", () => {
+        const WooCommerce = createWooCommerceApi(
+          "https://test-url.com",
+          "test consumer key",
+          "test consumer secret"
+        );
+
+        expect(WooCommerce).toMatchObject({
+          url: "https://test-url.com",
+          consumerKey: "test consumer key",
+          consumerSecret: "test consumer secret",
+        });
       });
     });
   });
+
+  // describe("getProducts", () => {
+  //   describe("Success", () => {
+  //     beforeAll(() => {
+  //       hydrateEnv();
+  //     });
+
+  //     it("should return the products", async () => {
+  //       const products = await getProducts();
+  //       console.log("products in test", products);
+
+  //       expect(1).toBe(1);
+  //     });
+  //   });
+  // });
 });
